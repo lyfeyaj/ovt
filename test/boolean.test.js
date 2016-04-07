653512fd59954aa1101bf676c9f2255f1153405f
lyfeyaj <lyfeyaj@gmail.com>
2016-04-07 23:52:09 +0800

    add boolean tests and fix boolean bugs

---
+++ test/boolean.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Helper = require('./helpers');
+const BooleanType = require('../lib/types/boolean');
+
+describe('BooleanType', function() {
+  let schema;
+
+  beforeEach(function() {
+    schema = (new BooleanType()).required.isBoolean;
+  });
+
+  describe('isBoolean()', function() {
+    it('should validate valid values', function() {
+      Helper.validate(schema, [
+        [undefined, false],
+        [null, false],
+        [0, false],
+        [1, false],
+        [true, true],
+        [false, true]
+      ], { convert: false });
+    });
+  });
+});
