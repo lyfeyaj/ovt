7bf6bea4587d2d173de2059096b17a9874fa0e76
lyfeyaj <lyfeyaj@gmail.com>
2016-04-08 10:20:27 +0800

    add object type tests

--- test/helpers.js
+++ test/helpers.js
@@ -8,14 +8,16 @@ module.exports = {
     samples.forEach(function(sample) {
       var value = sample[0];
       var result = sample[1];
-      if (result) {
+      if (result === true) {
         expect(function() {
           Ovt.assert(value, schema, options);
         }).to.not.throw(Error);
-      } else {
+      } else if (result === false) {
         expect(function() {
           Ovt.assert(value, schema, options);
         }).to.throw(Error);
+      } else {
+        expect(Ovt.validate(value, schema, options)).to.have.deep.property('value').to.eql(result);
       }
     });
   }
---
+++ test/object.test.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const Helper = require('./helpers');
+const ObjectType = require('../lib/types/object');
+const ArrayType = require('../lib/types/array');
+const StringType = require('../lib/types/string');
+
+describe('ObjectType', function() {
+  let schema;
+
+  beforeEach(function() {
+    schema = (new ObjectType()).isObject;
+  });
+
+  describe('isObject()', function() {
+    it('should validate valid values', function() {
+      Helper.validate(schema, [
+        [null, false],
+        [0, false],
+        ['', false],
+        [NaN, false],
+        [[], true],
+        [new Object(), true],
+        [{}, true]
+      ], { convert: false });
+    });
+  });
+
+  describe('rename()', function() {
+    it('should rename old key to new key', function() {
+      Helper.validate(schema.rename('name', 'nickname'), [
+        [{ name: 'Felix Liu' }, { nickname: 'Felix Liu' }]
+      ], { convert: false });
+    });
+
+    it('should rename according to renames', function() {
+      Helper.validate(schema.rename({
+        name: 'nickname',
+        sex: 'gender'
+      }), [
+        [{ name: 'Felix Liu', sex: 'male' }, { nickname: 'Felix Liu', gender: 'male' }]
+      ], { convert: false });
+    });
+  });
+
+  describe('keys()', function() {
+    it('should validate valid values', function() {
+      Helper.validate(schema.keys({
+        name: (new StringType).isString.required.valid('Felix'),
+        hobbies: (new ArrayType).isArray.required.items((new StringType).isString.required)
+      }).keys({
+        gender: (new StringType).isString.required.only(['male', 'femaile', 'unknown'])
+      }), [
+        [{ name: 'Jill' }, false],
+        [{ name: 'Felix' }, false],
+        [{ name: 'Felix', hobbies: [] }, false],
+        [{ name: 'Felix', hobbies: ['pingpong'] }, false],
+        [{ name: 'Felix', hobbies: ['pingpong'], gender: 'boy' }, false],
+        [{ name: 'Felix', hobbies: ['pingpong'], gender: 'male' }, true]
+      ], { convert: false });
+    });
+  });
+
+  describe('add()', function() {
+    it('should validate valid values', function() {
+      Helper.validate(
+        schema
+          .add('name', (new StringType).isString.required.valid('Felix'))
+          .add('hobbies', (new ArrayType).isArray.required.items((new StringType).isString.required))
+          .add('gender', (new StringType).isString.required.only(['male', 'femaile', 'unknown']))
+      ,
+        [
+          [{ name: 'Jill' }, false],
+          [{ name: 'Felix' }, false],
+          [{ name: 'Felix', hobbies: [] }, false],
+          [{ name: 'Felix', hobbies: ['pingpong'] }, false],
+          [{ name: 'Felix', hobbies: ['pingpong'], gender: 'boy' }, false],
+          [{ name: 'Felix', hobbies: ['pingpong'], gender: 'male' }, true]
+        ],
+        { convert: false }
+      );
+    });
+  });
+});
